d9cd27b890492a7f02ab20f0d945676ffa436ab8
aOne1or-aZer0 <asahi_010101@protonmail.com>
2024-08-26 02:43:38 -0500

    Programando en C#

--- 5thEdition/Level08-Console2.0/TheDefenseOfConsolas/Program.cs
+++ 5thEdition/Level08-Console2.0/TheDefenseOfConsolas/Program.cs
@@ -1,4 +1,10 @@
-﻿/* */
+﻿/*Objetivos: 
+Preguntar al usuario por la fila y columna objetivo.
+- Calcular las filas y columnas.
+- Mostrar las instrucciones de despliegue en un color diferente.
+- Cambiar el título de la ventana.
+- Reproducir un sonido con Console.Beep() al final.
+*/
 namespace TheDefenseOfConsolas
 {
   public class Program
@@ -8,20 +14,26 @@ namespace TheDefenseOfConsolas
       Console.Title = "The Defense Of Consolas";
       Console.WriteLine("CIUDAD CONSOLAS - SISTEMA DE DEFENSA MAGICA");
       Console.WriteLine("--------------------------------------------");
-     
+     // Entrada de datos
       Console.Write("Ingresa el numero de la fila objetivo (1-8): ");
-      string inputNumeroFila = Console.ReadLine();
-      byte numeroFila = Convert.ToByte(inputNumeroFila);
+      string inputFila = Console.ReadLine();
+      int fila = Convert.ToInt32(inputFila);
 
       Console.Write("Ingresa el numero de la columna objetivo (1-8): ");
-      string inputNumeroColumna = Console.ReadLine();
-      byte numeroColumna = Convert.ToByte(inputNumeroColumna);
-
-      Console.WriteLine("Presiona cualquier tecla para desplegar el escuadron");
+      string inputColumna = Console.ReadLine();
+      int columna = Convert.ToInt32(inputColumna);
+      // Cambia el color del texto de la consola
+      Console.ForegroundColor = ConsoleColor.Green;
+      // Imprime en pantalla las instrucciones
+      Console.WriteLine("Presiona cualquier tecla para desplegar el escuadron...");
       Console.ReadKey(true);
-      Console.Beep(440, 1000);
+      Console.WriteLine($"{fila}, {columna - 1}");
+      Console.WriteLine($"{fila - 1}, {columna}");
+      Console.WriteLine($"{fila}, {columna + 1}");
+      Console.WriteLine($"{fila + 1}, {columna}");
+      Console.ForegroundColor = ConsoleColor.White;
+      Console.Beep(900, 300);
     }
   }
-    
 }
 
---
+++ 5thEdition/Level09-DecisionMaking/RepairingTheClocktower/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Objetivos:
+-Toma un número como input/entrada desde la consola.
+- Se muestra en pantala la palabra «Tick» si el número es par, «Tock» si el número es impar.
+*/
+using System;
+namespace RepairingTheClockTower
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            // Entrada de datos
+            Console.WriteLine("Reparando el reloj");
+            Console.WriteLine("-------------------");
+            Console.Write("Ingresa un número: ");
+            string? inputDelUsuario = Console.ReadLine();
+            int numeroIngresado = Convert.ToInt32(inputDelUsuario);
+            // Condicional
+            if (numeroIngresado % 2 == 0)
+            {
+                Console.WriteLine("El número es par"); // Todo número divisible entre 2 es par
+                Console.WriteLine("Tick"); // Imprime en pantalla 'Tick" si el número es par
+            }
+            else
+            {
+                Console.WriteLine("El número es impar");
+                Console.WriteLine("Tock"); 
+            }    
+        }
+    }
+}
