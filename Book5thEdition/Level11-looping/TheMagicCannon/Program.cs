b4035c683083cb0cec3ab95491936f21ef09f729
aOne1or-aZer0 <asahi_010101@protonmail.com>
2024-09-01 22:10:21 -0500

    Looping

--- Book5thEdition/Level11-looping/TheMagicCannon/Program.cs
+++ Book5thEdition/Level11-looping/TheMagicCannon/Program.cs
@@ -1,6 +1,7 @@
 ﻿/*Objetivos:
-
-
+- El programa debe recorrer los valores entre 1 y 100 y mostrar el tipo 
+- Cambia el color de la salida en función del tipo. (Por ejemplo, rojo para Fuego, amarillo para
+Eléctrico, y azul para Eléctrico y Fuego)
 */
 using System;
 namespace TheMagicCannon
@@ -11,19 +12,21 @@ namespace TheMagicCannon
         {
             Console.WriteLine("EL CAÑON MÁGICO");
             Console.WriteLine("----------------");
+            // Bucle
             for (int contador = 1; contador <= 100; contador++)
             {
-                if (contador % 3 == 0 && contador % 5 == 0)
+                // Se declaran los condicionales de la más especifica a la más general
+                if (contador % 3 == 0 && contador % 5 == 0) // Multiplos de 3 y 15
                 {
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine($"{contador}: Fuego y Electrico");
                 }
-                else if (contador % 5 == 0)
+                else if (contador % 5 == 0) // Multiplos de 5
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine($"{contador}: Amarillo");
                 }
-                 else if (contador % 3 == 0)
+                 else if (contador % 3 == 0) // Multiplos de 3
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine($"{contador}: Fuego");
@@ -31,7 +34,7 @@ namespace TheMagicCannon
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine($"{contador}: Normal");
+                    Console.WriteLine($"{contador}: Normal"); // 
                 }
                 Console.ForegroundColor = ConsoleColor.White;
             }
