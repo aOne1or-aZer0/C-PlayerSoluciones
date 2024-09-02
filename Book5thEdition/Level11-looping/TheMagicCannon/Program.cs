617b3d743def117e49e711f95a4b6799d54cd107
aOne1or-aZer0 <asahi_010101@protonmail.com>
2024-09-01 21:10:50 -0500

    Looping

--- Book5thEdition/Level11-looping/TheMagicCannon/Program.cs
+++ Book5thEdition/Level11-looping/TheMagicCannon/Program.cs
@@ -11,7 +11,30 @@ namespace TheMagicCannon
         {
             Console.WriteLine("EL CAÑON MÁGICO");
             Console.WriteLine("----------------");
-            
-        }
-    }
+            for (int contador = 1; contador <= 100; contador++)
+            {
+                if (contador % 3 == 0 && contador % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{contador}: Fuego y Electrico");
+                }
+                else if (contador % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{contador}: Amarillo");
+                }
+                 else if (contador % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{contador}: Fuego");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"{contador}: Normal");
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }     
+    }   
 }
