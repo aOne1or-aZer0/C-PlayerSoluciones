815b0b8a0763f6f3e7c0e1579e5887496666b6eb
aOne1or-aZer0 <asahi_010101@protonmail.com>
2024-09-09 19:42:48 -0500

    Libro Quinta Edición

---
+++ Book5thEdition/Level13-Methods/TakingaNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Objetivos:
+- Crear 2 métodos para usarlos en los programas de consola previos para mejorarlos
+*/
+namespace TakingaNumber
+{
+    public class Program
+    {
+        public void MostrarMensaje()
+        {
+            Console.Title = "Metodos";
+            Console.WriteLine("METODOS");
+            Console.WriteLine("--------");
+        }
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.MostrarMensaje();
+        }
+    }
+
+   
+
+}
